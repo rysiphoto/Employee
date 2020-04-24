d776ea3acb3f72068c6f4f25b3993a11d392b2c5
Ryan Siverson <rcsskier@mac.com>
2020-04-23 19:32:31 -0600

    rearranged question order

--- app.js
+++ app.js
@@ -10,10 +10,36 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+const teamMembers = [];
+const employee = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+
+
+function createEmployeeMenu() {
+    inquirer.prompt({
+
+        type: "list",
+        name: "role",
+        message: "Employee's Job Role: ",
+        choices: ["Engineer", "Manager", "Intern", "I am done adding employees."]
+
+    }).then(answers => {
+
+        switch (answers.employeeType) {
+            case "Manager":
+                return askManagerQuestions();
+            case "Engineer":
+                return askEngineerQuestions();
+            case "Intern":
+                return askInternQuestions();
+            case "Done":
+            default:
+
+        }
+    })
+};
 const generalQs = ([
     {
         type: "input",
@@ -57,98 +83,75 @@ const generalQs = ([
 
 // Question sets
 const managerQs = [...generalQs, {
-
+    type: "input",
+    name: "officeNumber",
+    message: "Office Number: ",
+    validate: function (value) {
+        if (value.length) {
+            return true;
+        } else {
+            return `Please enter the manager's office number: `;
+        }
+    }
 }];
-const engineerQs = [...generalQs, {
-
+const engineerQs = [...generalQs,
+{
+    type: "input",
+    name: "github",
+    message: "What is your GitHub Name? ",
+    validate: function (value) {
+        if (value.length) {
+            return true;
+        } else {
+            return `Please enter the engineer's GitHub user name: `;
+        }
+    }
 }];
 
 const internQs = [...generalQs, {
-
+    type: "input",
+    name: "school",
+    message: "School Name: ",
+    validate: function (value) {
+        if (value.length) {
+            return true;
+        } else {
+            return `Please enter the Intern's school name: `;
+        }
+    }
 }];
 
-
-function createEmployeeMenu() {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "role",
-            message: "Employee's Job Role: ",
-            choices: [
-                "Engineer",
-                "Manager",
-                "Intern",
-                "I am done adding employees."
-            ]
-
-        }.then(answers => {
-            switch (answers.employeeType) {
-                case "Manager":
-                    return askManagerQuestions();
-                case "Engineer":
-                    return askEngineerQuestions();
-                case "Intern":
-                    return askInternQuestions();
-                case "Done":
-
-            }
-        }),
-        ,
-
-        {
-            type: "input",
-            name: "school",
-            message: "School Name: ",
-            validate: function (value) {
-                if (value.length) {
-                    return true;
-                } else {
-                    return `Please enter the Intern's school name: `;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "What is your GitHub Name? ",
-            validate: function (value) {
-                if (value.length) {
-                    return true;
-                } else {
-                    return `Please enter the engineer's GitHub user name: `;
-                }
-            }
-
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Office Number: ",
-            validate: function (value) {
-                if (value.length) {
-                    return true;
-                } else {
-                    return `Please enter the manager's office number: `;
-                }
-            }
-        }
-
-
-    ]).then(answers => {
-        console.log(answers);
-        // if (this.role === Engineer) {
-        //     const newEngineer = new Engineer(answers.role, answers.name, answers.email, answers.id, answers.github);
-        // } if (this.role === Manager) {
-        //     const newManager = new Manager(answers.role, answers.name, answers.email, answers.id, answers.officeNumber);
-        // } if (this.role === Intern) {
-        //     const newIntern = new Intern(answers.role, answers.name, answers.email, answers.id, answers.school);
-        // } else {
-        //     return;
-        // }
+function askManagerQuestions() {
+    inquirer.prompt(managerQs).then(answers => {
+        const manager = new Manager(answers.name, answers.title, answers.id, answers.email, answers.officeNumber);
+        employee.push(manager);
+        createEmployeeMenu();
     })
-
+}
+function askInternQuestions() {
+    inquirer.prompt(internQs).then(answers => {
+        const intern = new Intern(answers.name, answers.title, answers.id, answers.email, answers.school);
+        employee.push(intern);
+        createEmployeeMenu();
+    })
+}
+function askEngineerQuestions() {
+    inquirer.prompt(engineerQs).then(answers => {
+        const engineer = new Engineer(answers.name, answers.title, answers.id, answers.email, answers.github);
+        employee.push(engineer);
+        createEmployeeMenu();
+    })
 }
 
+function buildTeam() {
+    // Create the output directory if the output path doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+}
+
+createEmployeeMenu();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
